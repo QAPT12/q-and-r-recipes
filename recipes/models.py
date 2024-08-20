406cd41a9c153a53f83008c1b24dda0897456a6e
QAPT12 <tuckquintin@hotmail.com>
2024-08-19 23:36:36 -0230

    Preliminary DB Tables and models set up along with basic routing and views

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Recipe(models.Model):
+    # Class for representing a recipe with included steps and rating
+    recipe_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    rating = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
+    date_added = models.DateField()
+
+class Steps(models.Model):
+    # Class to hold the step by step instructions for the recipes
+    step_id = models.AutoField(primary_key=True)
+    recipe_id = models.ForeignKey(Recipe, db_column="recipe_id", on_delete=models.CASCADE)
+    step_number = models.IntegerField(
+        validators=[
+            MaxValueValidator(1)
+        ]
+    )
+
+class Ingredients(models.Model):
+    ingredient_id = models.AutoField(primary_key=True)
+    recipe_id = models.ForeignKey(Recipe, db_column="recipe_id", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    quantity = models.CharField(max_length=200)
+    measurement = models.CharField(max_length=200)
+    
